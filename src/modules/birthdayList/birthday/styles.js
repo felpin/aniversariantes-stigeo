b058deda1969133eb1c5f4cc74810c263f09f92e
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2017-10-26 17:17:25 -0200

    Adds Birthday component

---
+++ src/modules/birthdayList/birthday/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  birthday: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  celebrator: {
+    fontSize: 18,
+  },
+  celebrators: {
+    flexGrow: 5,
+  },
+  day: {
+    flexGrow: 1,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
